3283c04c96b392a2048605c3735ae7c49d61a084
maquiles <mattaquiles@gmail.com>
2017-03-25 17:12:58 -0400

    dbconn.js
    file that will allow for connection to the database from the express
    server

---
+++ sp-node-mysql/dbconn.js
@@ -0,0 +1,15 @@
+/* AUTHOR: MATTHEW AQUILES
+** FINBUDDY; DUCKHACKS17
+** NOTES: INFORMATION TO CONNECT TO DATABSE; DATABASE IS ON MATTS MAC
+*/
+
+var mysql = require('mysql');
+
+var connection = mysql.createPool({
+  host:'localhost',
+  user:'root',
+  password:'p0o9I*U&', //should probably not hard code this, but its ok for now
+  database:'FinBuddy'
+});
+
+mondule.exports = connection;
