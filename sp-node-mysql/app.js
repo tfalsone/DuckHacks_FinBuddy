269b55cca624a7a0a6c13afceb0d99fbcaf9d7bb
maquiles <mattaquiles@gmail.com>
2017-03-25 17:58:30 -0400

    setting up www and app.js to include rest routes
    making sure the express server runs and is prepared to accept routes
    once they are written

--- sp-node-mysql/app.js
+++ sp-node-mysql/app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var index = require('./routes/index');
+//require routes defined in routes directory
 var users = require('./routes/users');
+//TODO: add routes for the rest of the tables here
 
 var app = express();
 
@@ -16,14 +18,14 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
+app.use(express.static(path.join(__dirname + '/FinBuddy_Bootstrap'))); //serves webapp
 app.use('/users', users);
+//TODO: include other urls for requests here
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
