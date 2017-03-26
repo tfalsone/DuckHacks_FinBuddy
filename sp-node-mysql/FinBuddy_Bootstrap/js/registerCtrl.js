16c5446d26bd596245d2ff594f364e477a68d03d
maquiles <mattaquiles@gmail.com>
2017-03-26 11:27:30 -0400

    resgistration
    able to register and post to users db

--- sp-node-mysql/FinBuddy_Bootstrap/js/registerCtrl.js
+++ sp-node-mysql/FinBuddy_Bootstrap/js/registerCtrl.js
@@ -1,24 +1,24 @@
-/* global angular, $ */
+var registerApp = angular.module("registerApp", ['ngRoute']);
 
-var registerApp = angular.module("registerApp", ["ngRoute"]);
-registerApp.controller("registerCtrl", function ($scope, $html) {
+registerApp.controller('registerCtrl', function($scope, $http) {
     "use strict";
-    $scope.submitReg = function () {
-        console.log("We are in the function");
-        var data = {};
-        data["user_id"] = document.getElementById("email").value;
-        data["password"] = document.getElementById("password").value;
-        data["name"] = document.getElementById("name").value;
-        data["create_date"] = new Date();
-    };
+    var mydata = {};
 
-    $http.post("http://155.246.213.64:3000/users", data).success(function (data) {
+    $scope.submitReg = function() {
+        console.log("We are in the function");
+        mydata["user_id"] = document.getElementById("email").value;
+        mydata["password"] = document.getElementById("password").value;
+        mydata["name"] = document.getElementById("name").value;
+        //data["create_date"] = new Date();
+
+    $http.post("http://155.246.213.64:3000/users", mydata).success(function (data) {
         if (data.error) {
-            console.log("Error: ");
+            console.log("Error:");
             console.log(data);
         } else {
             console.log("Success");
             console.log(data);
         }
     });
+  }
 });
