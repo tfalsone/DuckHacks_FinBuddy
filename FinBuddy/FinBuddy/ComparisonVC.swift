0969e680ad1ba3524dc65b0140f50106d2b1b05f
Gregory Johnson <gsoccer111@gmail.com>
2017-03-26 14:01:46 -0400

    Comparison controller
    viewing comparison after changes is now set up

--- FinBuddy/FinBuddy/ComparisonVC.swift
+++ FinBuddy/FinBuddy/ComparisonVC.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 class ComparisonVC: UIViewController {
-    
+    @IBOutlet weak var s_living: UISlider!
+    @IBOutlet weak var s_food: UISlider!
+    @IBOutlet weak var s_savings: UISlider!
+    @IBOutlet weak var l_living: UILabel!
+    @IBOutlet weak var l_food: UILabel!
+    @IBOutlet weak var l_savings: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.s_living.value = Float(myData.percLiving)
+        self.l_living.text = "\(myData.percLiving)%"
+        self.s_food.value = Float(myData.percFood)
+        self.l_food.text = "\(myData.percFood)%"
+        self.s_savings.value = Float(myData.percSavings)
+        self.l_savings.text = "\(myData.percSavings)%"
 
         // Do any additional setup after loading the view.
     }
