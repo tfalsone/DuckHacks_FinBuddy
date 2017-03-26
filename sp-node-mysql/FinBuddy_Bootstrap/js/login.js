e268b8d68690a614a1f6b167d023285947a73f59
HunterBartholomew <hbarthol@stevens.edu>
2017-03-26 13:38:57 -0400

    login page

--- sp-node-mysql/FinBuddy_Bootstrap/js/login.js
+++ sp-node-mysql/FinBuddy_Bootstrap/js/login.js
@@ -1,11 +1,18 @@
-var login_page = angular.module('login', ['ngRoute']);
-
-login_page.controller('login', function($scope, $http){
-  "use strict";
-
-  //TODO: need to update this url to get from whatever account is signed in
-  $http.get('http://155.246.213.64:3000/budget_data/' + localStorage["user_id"])
-    .success(function(data){
-      
+$("#myForm").submit(function(e){
+     e.preventDefault();
+    $.get('http://155.246.213.64:3000/users/' + document.getElementById('email').value, function(response){
+        console.log(response);
+        if(response.length === 0){
+            alert("Username or password entered is incorrect.");
+        }
+        else{
+            if(document.getElementById("password").value == response[0]['password']){
+                localStorage.setItem("user_id", document.getElementById('email').value);
+                window.location.href = "dashboard.html";
+            }
+            else{
+                alert("Username or password entered is incorrect.");
+            }
+        }
     });
 });
